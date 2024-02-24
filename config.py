b23d7898ea2941b4c86f27cc06100fe0b4a27664
Rockas888 <105117889+Rockas888@users.noreply.github.com>
2024-02-25 02:20:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = "6518790189:AAH18Rn2TSpRV2cDrFVF-Ruqy9CrChZ-F1s"
+    BOT_TOKEN = "6915618928:AAGS7D5D9WeIn1iSm6c7LXtppE2pM_csQUY"
     API_ID = "20717621"
     API_HASH = "a68af155e7f0c79b1baab1f185f4181d"
-    AUTH_USERS = '5934830127'
+    AUTH_USERS = '6585878012'
 
